87cf6e57fa0d7b78375403fc29ba0ae9dcf6dc10
Karina Montiel <karinamontiel375@gmail.com>
2019-04-19 01:05:18 +0000

    CSS and display of title

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,34 @@
-var platform = new H.service.Platform({
-    'app_id':'{iTGYNykOJsf7F7Da6wb5}',
-    'app_code':'{rmsp-mvgzIMNSMWeIdBgoA}'
+ /*global $*/
+ 
+//  $( document ).ready(function() {
+//     console.log( "ready!" );
+
+ $("#searchb").click(function() {
+     console.log("searchb");
+ 
+    let movie= $("#input1").val();
+
+    $.ajax({
+     
+     
+        url: `https://api.themoviedb.org/3/search/movie?api_key=a940e759d1db2087736ae02016ec33b2&query=${movie}`,
+        method: "GET",
+        success: function(response) {
+            console.log("success");
+            console.log(response.results[0].original_title);
+            $("#title").append(response.results[0].original_title);
+            $("#over").append(response.results[0].overview);
+            $("#release").append(response.results[0].release_date);
+            
+            
+            
+        },
+        error: function(xhr,status,error) {
+            console.log("Error");
+            console.log(error);
+        }
+    }); 
     
-});
+    
+ });
+// });
