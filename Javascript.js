fc513630ab8873372494245c60f4b2e4b01f2e68
Karina Montiel <karinamontiel375@gmail.com>
2019-05-03 01:16:01 +0000

    Drama HTML

--- Javascript.js
+++ Javascript.js
@@ -32,3 +32,31 @@
     
  });
 // });
+
+
+$("#dropdownMenuButton").click(function() {
+    $(".dropdown-menu").toggleClass("show");
+});
+
+
+
+// JS for the Drama HMTL
+
+$( document ).ready(function() {
+     $.ajax({
+     
+     
+        url: `https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=a940e759d1db2087736ae02016ec33b2`,
+        method: "GET",
+        success: function(response) {
+            console.log("success");
+            console.log(response.results[0].original_title);
+           
+            
+            
+        },
+        error: function(xhr,status,error) {
+            console.log("Error");
+            console.log(error);
+        }
+});
